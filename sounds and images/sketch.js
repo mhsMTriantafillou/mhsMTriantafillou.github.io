1fdcac748512471dc35dbaf3b7b99ac7d83a157b
mhsMTriantafilou <48797363+mhsMTriantafilou@users.noreply.github.com>
2019-04-23 10:43:23 -0400

    Add files via upload

---
+++ sounds and images/sketch.js
@@ -0,0 +1,9 @@
+let img;
+function preload() {
+  img = loadImage('apple_PNG1234.png');
+}
+function setup() {
+  // Top-left corner of the img is at (0, 0)
+  // Width and height are the img's original width and height
+  image(img, 0, 0);
+}
