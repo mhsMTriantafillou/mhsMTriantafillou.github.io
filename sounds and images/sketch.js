6c9b32873e9e703785c01114da44743898fd13a2
mhsMTriantafillou <48797363+mhsMTriantafillou@users.noreply.github.com>
2019-04-24 08:56:24 -0400

    Update sketch.js

--- sounds and images/sketch.js
+++ sounds and images/sketch.js
@@ -5,5 +5,5 @@ function preload() {
 function setup() {
   // Top-left corner of the img is at (0, 0)
   // Width and height are the img's original width and height
-  image(img, 0, 0);
+  image(img, 0, 0,100,100);
 }
