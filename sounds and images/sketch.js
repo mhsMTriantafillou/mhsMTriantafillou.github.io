793ef661426d4a25539dcb812bd178b03878e8d8
mhsMTriantafillou <48797363+mhsMTriantafillou@users.noreply.github.com>
2019-04-25 09:48:06 -0400

    Update sketch.js

--- sounds and images/sketch.js
+++ sounds and images/sketch.js
@@ -1,9 +1,14 @@
 let img;
+let song;
 function preload() {
-  img = loadImage('apple_PNG12423.png');
+//making apple
+  img = loadImage('apple_PNG1234.png');
+  song= loadSound('Apple_Bite-Simon_Craggs-168364397.mp3');
 }
 function setup() {
   // Top-left corner of the img is at (0, 0)
   // Width and height are the img's original width and height
-  image(img, 0, 0,100,100);
+  image(img, 0, 0);
+ song.setVolume(0.1)
+ sound.play();
 }
