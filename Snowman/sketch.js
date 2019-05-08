4fc498f2af16a8998f3d45e312a226136d1baa12
mhsMTriantafillou <48797363+mhsMTriantafillou@users.noreply.github.com>
2019-05-08 08:27:13 -0400

    Update sketch.js

--- Snowman/sketch.js
+++ Snowman/sketch.js
@@ -1,3 +1,7 @@
+//Global Variables
+let  s= 155;
+
+
 function setup() {
   // put setup code here
   createCanvas(800,600);
@@ -6,14 +10,13 @@ function setup() {
 }
 
 function draw() {
-  let  s= 155;
+// making the circles
 
-  // making the circles
-ellipse(s+150,s+300,s+100);
-ellipse(s+150, s+150, s+60);
-ellipse(s+150,s,s+25);
+  ellipse(s+150,s+300,s+100);
+  ellipse(s+150, s+150, s+60);
+  ellipse(s+150,s,s+25);
 //making snowmans arms
-line(250,325,100,250);
-line(350,325,500,250);
+  line(250,325,100,250);
+  line(350,325,500,250);
 
 }
