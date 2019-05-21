c6e88ef5f3009c015c2689a2f54f565fd794e8ca
mhsMTriantafillou <48797363+mhsMTriantafillou@users.noreply.github.com>
2019-05-21 10:15:20 -0400

    Update sketch.js

--- mouse and keyboard/sketch.js
+++ mouse and keyboard/sketch.js
@@ -1,3 +1,5 @@
+/*Using the mouse
+Maki triantafillou, May 21 2019*/
 
 //global variables
 let value = 0;
@@ -8,29 +10,28 @@ let xSpeed = 5;
 let ySpeed = 5;
 
 let size = 50;
+
 function setup(){
 createCanvas(windowWidth,windowHeight);
 fill(0,255,0);
-}
+}//End of setup
 
 function draw(){
   textSize(100)
   text('Hold mouse for circles :)', 150, 400)
-if (mouseIsPressed) {
-if (x<=width){
-  ellipse(mouseX, mouseY, 50, 50);
+    if (mouseIsPressed) {
+    if (x<=width){
+      ellipse(mouseX, mouseY, 50, 50);
 }
 }
 }
 //made rainbow squares
 function mouseMoved() {
-
   if (x<=width) {
-rect(mouseX,mouseY, size/2,size/2);
-
-noStroke();
-fill(random(255),random(255),random(255));
-
-  }
+    rect(mouseX,mouseY, size/2,size/2);
+    noStroke();
+    fill(random(255),random(255),random(255));
+}
 
 }
+//End of function draw
