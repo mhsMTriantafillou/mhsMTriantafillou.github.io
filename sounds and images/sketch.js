4597b2fdb1004ef0f44808b97819befe57c0d313
mhsMTriantafillou <48797363+mhsMTriantafillou@users.noreply.github.com>
2019-05-21 10:36:27 -0400

    Update sketch.js

--- sounds and images/sketch.js
+++ sounds and images/sketch.js
@@ -1,27 +1,35 @@
+/*Sounds and images
+By:Maki Triantafillou
+May 21, 2019*/
+
+//Global variables
 let img;
 let mySound;
 function preload() {
 //making apple
-  img = loadImage('apple_PNG12423.png');
-  mySound= loadSound('Biting Apple-SoundBible.com-415478302.mp3');
+img = loadImage('apple_PNG12423.png');
+mySound= loadSound('Biting Apple-SoundBible.com-415478302.mp3');
 }
+
 function setup() {
   // Top-left corner of the img is at (0, 0)
   // Width and height are the img's original width and height
   createCanvas(800,600);
-}
+}//End of setup
 
 function draw(){
   image(img, 0,0,500,500);
 }
-
+//end of draw
 function mousePressed() {
   if (mySound.isPlaying()) {
     // .isPlaying() returns a boolean
     mySound.stop();
     
-  } else {
+} 
+  else {
     mySound.play();
     
-  }
 }
+}
+//end of code
