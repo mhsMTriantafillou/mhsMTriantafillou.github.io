67d8292aa0bd392051448324686544f138bc4dd7
mhsMTriantafillou <48797363+mhsMTriantafillou@users.noreply.github.com>
2019-05-21 10:48:31 -0400

    Update sketch.js

--- decision structure/sketch.js
+++ decision structure/sketch.js
@@ -1,3 +1,7 @@
+/*Decision structure
+by:Maki Triantafillou
+May 21, 2019*/
+
 //global variables
 let choices = ['Rock','Paper','Scissors']
 let cpuChoice;
@@ -14,24 +18,26 @@ let s;
 let tic;
 let score;
 let moana;
-function preload(){
-rock=loadImage('rock.png');
-scissors=loadImage('scissors.png')
-paper=loadImage('paper.png')
-choose=loadImage('choose.png')
-p=loadImage('P.png')
-r=loadImage('R.png')
-s=loadImage('S.png')
-tic=loadImage('tic.png')
-score=loadImage('score.png')
-moana=loadImage('moana.png')
-sad=loadImage('sad.png')
-}
-function setup(){
-createCanvas(windowWidth,windowHeight);
-background(155);
 
-}
+function preload(){
+  rock=loadImage('rock.png');
+  scissors=loadImage('scissors.png')
+  paper=loadImage('paper.png')
+  choose=loadImage('choose.png')
+  p=loadImage('P.png')
+  r=loadImage('R.png')
+  s=loadImage('S.png')
+  tic=loadImage('tic.png')
+  score=loadImage('score.png')
+  moana=loadImage('moana.png')
+  sad=loadImage('sad.png')
+}//end of preload
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  background(155);
+
+}//end of setup
 
 function draw(){
   if (keyPress === false) {
@@ -55,46 +61,46 @@ function keyPressed(){
   if (keyCode === 82){
     keyPress = true;
     print("User Choice:",'Rock');
-    if (cpuChoice === 'Rock'){
-      background(155);
-      textSize(50);
-      textFont('Georgia')
-      text('I guess nobody wins',300,500);
-      image(tic,500,600,200,200);
-      image(score,700,100,300,300);
-   }
-    else if (cpuChoice === 'Paper'){
-      background(255,0,0);
-      textSize(50);
-      textFont('Georgia');
-      text('YOU LOSE!', 300,300);
+  if (cpuChoice === 'Rock'){
+    background(155);
+    textSize(50);
+    textFont('Georgia')
+    text('I guess nobody wins',300,500);
+    image(tic,500,600,200,200);
+    image(score,700,100,300,300);
+ }
+   else if (cpuChoice === 'Paper'){
+    background(255,0,0);
+    textSize(50);
+    textFont('Georgia');
+    text('YOU LOSE!', 300,300);
     image(sad,400,500,300,300);
   }
- else if(cpuChoice=== 'Scissors'){
-background(0,255,0);
-textSize(50);
-textFont('Georgia');
-text('winner winner chicken dinner',300,300);
-image(moana,200,550,700,700);
- }
+   else if(cpuChoice=== 'Scissors'){
+    background(0,255,0);
+    textSize(50);
+    textFont('Georgia');
+    text('winner winner chicken dinner',300,300);
+    image(moana,200,550,700,700);
+     }
 
   }
   if (keyCode === 80){
     keyPress = true;
     print("User Choice:",'Paper');
-    if (cpuChoice === 'Paper'){
-      background(155);
-      textSize(50);
-      textFont('Georgia')
-      text('I guess nobody wins',300,500);
-      image(tic,500,600,200,200);
-      image(score,700,100,300,300);
+  if (cpuChoice === 'Paper'){
+    background(155);
+    textSize(50);
+    textFont('Georgia')
+    text('I guess nobody wins',300,500);
+    image(tic,500,600,200,200);
+    image(score,700,100,300,300);
    }
-    else if (cpuChoice === 'Scissors'){
-      background(255,0,0);
-      textSize(50);
-      textFont('Georgia');
-      text('YOU LOSE!', 300,300);
+  else if (cpuChoice === 'Scissors'){
+    background(255,0,0);
+    textSize(50);
+    textFont('Georgia');
+    text('YOU LOSE!', 300,300);
     image(sad,400,500,300,300);
   }
   else if(cpuChoice=== 'Rock'){
@@ -106,9 +112,9 @@ image(moana,200,550,700,700);
 
 }
 }
-if (keyCode === 83){
-  keyPress = true;
-  print("User Choice:",'Scissors');
+  if (keyCode === 83){
+    keyPress = true;
+    print("User Choice:",'Scissors');
   if (cpuChoice === 'Scissors'){
     background(155);
     textSize(50);
@@ -122,14 +128,14 @@ if (keyCode === 83){
     textSize(50);
     textFont('Georgia');
     text('YOU LOSE!', 300,300);
-  image(sad,400,500,300,300);
+    image(sad,400,500,300,300);
 }
-else if(cpuChoice=== 'Paper'){
-background(0,255,0);
-textSize(50);
-textFont('Georgia');
-text('winner winner chicken dinner',300,300);
-image(moana,200,550,700,700);
+  else if(cpuChoice=== 'Paper'){
+    background(0,255,0);
+    textSize(50);
+    textFont('Georgia');
+    text('winner winner chicken dinner',300,300);
+    image(moana,200,550,700,700);
 }
 }
 }
