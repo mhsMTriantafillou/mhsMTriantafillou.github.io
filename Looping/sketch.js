017f3dd074a3450c675d2821914eabeeec5177c3
mhsMTriantafillou <48797363+mhsMTriantafillou@users.noreply.github.com>
2019-05-22 08:20:20 -0400

    Update sketch.js

--- Looping/sketch.js
+++ Looping/sketch.js
@@ -1,28 +1,34 @@
+/*Looping
+Maki Triantafillou
+May 22, 2019*/
+
+//global variables
 let x=0;
 let y=100;
 let size=50;
+
 function setup(){
-createCanvas(windowWidth,windowHeight);
-background(255);
-}
+  createCanvas(windowWidth,windowHeight);
+  background(255);
+}//end of setup
 
 function draw(){
-var x=0
-for (var x=0; x<=width; x=x+50){
+  var x=0
+  for (var x=0; x<=width; x=x+50){
 
-fill(0,random(255),0);
-ellipse(x,random(windowWidth),size,size);
-
-
-
-fill(random(255));
-rect(x,random(windowWidth),size,size);
+    fill(0,random(255),0);
+    ellipse(x,random(windowWidth),size,size);
 
 
-textSize(25);
-fill(0);
-text(char(random(255)),random(windowWidth),random(windowHeight));
 
-}
+    fill(random(255));
+    rect(x,random(windowWidth),size,size);
+
+
+    textSize(25);
+    fill(0);
+    text(char(random(255)),random(windowWidth),random(windowHeight));
 
 }
+
+}//end of draw
